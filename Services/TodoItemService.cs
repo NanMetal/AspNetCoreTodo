81b8a06bf07d258044b997f4cb27fc7ebe5fa6f2
Hernan <cuppispineta@live.com.ar>
2018-09-04 21:09:01 -0300

    Add TodoItem service and database context

---
+++ Services/TodoItemService.cs
@@ -0,0 +1,23 @@
+﻿using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Services
+{
+    public class TodoItemService : ITodoItemService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            return await _context.Items.Where(x => x.IsDone == false).ToArrayAsync();
+        }
+    }
+}
